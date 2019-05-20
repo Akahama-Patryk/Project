23713fc4b5ab9030a552ae9df1339aa214eee089
Akahama-Patryk <orel971111@gmail.com>
2019-05-20 23:08:55 +0200

    It's a test

--- view/admin/client/dashboard_admin_client.php
+++ view/admin/client/dashboard_admin_client.php
@@ -16,8 +16,9 @@ if (isset($_GET['ID']) && !empty($_GET['ID'])) {
 $dataClient = new User();
 $result = $dataClient->fetchClientData();
 ?>
-<html>
+<html lang="nl">
 <head>
+    <title>Project Supermarkt --Dashboard Admin:Client--</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
