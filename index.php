4888b783b46b7808b81e123dabe27b33de7c3f27
Akahama-Patryk <orel971111@gmail.com>
2019-07-02 00:13:57 +0200

    SESSION test shit

--- index.php
+++ index.php
@@ -39,6 +39,26 @@ if (!isset($_GET['module'])) {
     }
 }
 $results2 = $products->GetCategory();
+
+function getUserIpAddr()
+{
+    if (!empty($_SERVER['HTTP_CLIENT_IP']))   //check ip from share internet
+    {
+        $ip=$_SERVER['HTTP_CLIENT_IP'];
+    }
+    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))   //to check ip is pass from proxy
+    {
+        $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+    }
+    else
+    {
+        $ip=$_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
+}
+$ip = getUserIpAddr();
+var_dump($_SERVER['HTTP_USER_AGENT']);
+var_dump($ip);
 ?>
 <!doctype HTML>
 <html lang="nl">
